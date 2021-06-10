f01f58bd142ffe163a3ab324f265f9efd444b8db
Fzshuai <156117106@qq.com>
2021-06-10 14:20:13 +0800

    第一次提交

---
+++ src/main/java/com/fzshuai/gitstudy/GitstudyApplication.java
@@ -0,0 +1,13 @@
+package com.fzshuai.gitstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fzshuai/gitstudy/GitstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fzshuai.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
