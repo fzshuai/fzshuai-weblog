0b287fb1eb5181cd6d9c731550cbf5f4c679b3a9
fzshuai <156117106@qq.com>
2023-10-20 23:26:28 +0800

    定时任务更新文章浏览量，改为批量更新

--- fzshuai-job/src/main/java/com/fzshuai/job/service/BlogService.java
+++ fzshuai-job/src/main/java/com/fzshuai/job/service/BlogService.java
@@ -12,7 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.redisson.client.protocol.ScoredEntry;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import static com.fzshuai.common.constant.RedisConstant.ARTICLE_VIEWS_COUNT;
 
@@ -35,17 +37,18 @@ public class BlogService {
      */
     @XxlJob("updateBlogViewJobHandler")
     public void updateBlogViewJobHandler() throws Exception {
-        log.info("开始将缓存中数据更新到数据库...");
+        log.info("开始将缓存中的文章浏览量数据更新到数据库...");
         Collection<ScoredEntry<Object>> zSetEntryRange = RedisUtils.getZSetEntryRange(ARTICLE_VIEWS_COUNT, 0, -1);
-        zSetEntryRange.forEach(item -> {
-            ArticleVO articleVO = baseMapper.selectVoById((Long) item.getValue());
-            articleVO.setViewCount(item.getScore().intValue());
-            baseMapper.update(null,
-                new LambdaUpdateWrapper<Article>()
-                    .eq(Article::getArticleId, (Long) item.getValue())
-                    .set(Article::getViewCount, item.getScore().intValue())
-            );
-        });
-        log.info("更新完毕");
+        List<Article> articleList = new ArrayList<>();
+        zSetEntryRange.forEach(
+            item -> {
+                ArticleVO articleVO = baseMapper.selectVoById((Long) item.getValue());
+                articleVO.setViewCount(item.getScore().intValue());
+                Article article = BeanUtil.toBean(articleVO, Article.class);
+                articleList.add(article);
+            }
+        );
+        baseMapper.updateBatchById(articleList, 1000);
+        log.info("更新文章浏览量结束");
     }
 }
