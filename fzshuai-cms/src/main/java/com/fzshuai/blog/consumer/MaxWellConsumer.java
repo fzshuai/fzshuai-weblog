0fe5f8725b0418da7829e0c22e8366b291a736db
fzshuai <156117106@qq.com>
2024-02-19 19:48:45 +0800

    update 优化 优化导包

--- fzshuai-cms/src/main/java/com/fzshuai/blog/consumer/MaxWellConsumer.java
+++ fzshuai-cms/src/main/java/com/fzshuai/blog/consumer/MaxWellConsumer.java
@@ -1,7 +1,6 @@
 package com.fzshuai.blog.consumer;
 
 import com.alibaba.fastjson.JSON;
-
 import com.fzshuai.blog.domain.Article;
 import com.fzshuai.blog.domain.dto.ArticleSearchDTO;
 import com.fzshuai.blog.domain.dto.MaxwellDataDTO;
@@ -9,11 +8,9 @@ import com.fzshuai.blog.mapper.ElasticsearchMapper;
 import com.fzshuai.common.utils.BeanCopyUtils;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.Objects;
 
 import static com.fzshuai.blog.constant.MQConstant.MAXWELL_QUEUE;
 
